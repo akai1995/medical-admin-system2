4a338cd947a8af5c3f1293a62ca77f9b3cbc6b75
akai1995 <1471519969@qq.com>
2026-05-04 14:08:50 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/medical-admin-system/',
+  base: './',
   css: {
     preprocessorOptions: {
       less: {
